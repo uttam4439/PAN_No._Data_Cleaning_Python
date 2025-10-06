a8dfb80f1a2d97d8289a4bf20fb0be6449cc0a72
UTTAM KUMAR <rajuttam4439@gmail.com>
2025-10-07 02:10:48 +0530

    Null Value identified

--- Validataion.py
+++ Validataion.py
@@ -8,3 +8,9 @@ print("Total No. of Data = ", len(datafile)) # total no. of data
 # Change dataset datatype object to string and then operation for remove spaces and make uppercase
 datafile["Pan_Numbers"] = datafile["Pan_Numbers"].astype('string').str.strip().str.upper()
 print(datafile.head(10))
+
+# Python or pandas behave differently with null value 
+print(datafile[datafile["Pan_Numbers"] == ''])
+
+# To get actual no. of null values contain by dataset
+print(datafile[datafile["Pan_Numbers"].isna()])
