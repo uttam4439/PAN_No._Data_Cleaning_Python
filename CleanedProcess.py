9ac1f3c57762a94efc88584fe08e14121412f888
UTTAM KUMAR <rajuttam4439@gmail.com>
2025-10-08 01:21:19 +0530

    Sequence Function

--- CleanedProcess.py
+++ CleanedProcess.py
@@ -17,4 +17,17 @@ def has_adjacent_rep(pan):
     for i in range(len(pan)-1):
         if(pan[i] == pan[i+1]):
             return True
-        return False
+    return False
+
+print("ABCDX ",has_adjacent_rep('ABCDX')) 
+print("AABDC ",has_adjacent_rep('AABDC'))
+
+''' Creating a function for checking it form a Character sequence or not'''
+def has_sequence(pan):
+    for i in range(len(pan)-1):
+        if ord(pan[i+1]) - ord(pan[i]) != 1:
+            return False
+    return True
+
+print("ABCDE ",has_sequence('ABCDE'))
+print("ABCDE ",has_sequence('ABCDF'))
